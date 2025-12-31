684616501b29f741763aad5cb0545677febaa2f0
Kushal9753 <kushalsahu352@gmail.com>
2025-12-31 23:12:33 +0530

    remove old mongo url and use env variable

--- dbconfig.js
+++ dbconfig.js
@@ -1,17 +1,3 @@
-// import { MongoClient } from "mongodb";
-
-// const url ="mongodb+srv://Kushal:kushal12345@cluster0.z86e0ss.mongodb.net/?appName=Cluster0";
-
-// const dbName = "node-project"
-
-// export const collectionName = "todo";
-
-// const client = new MongoClient(url);
-
-// export const connection = async () => {
-//   await client.connect();
-//   return await client.db(dbName);
-// };
 
 
 import { MongoClient } from "mongodb";
